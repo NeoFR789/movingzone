f3f400b7a4c1638a75a33a0113bcb3a803400c7b
NeoFR789 <46304126+NeoFR789@users.noreply.github.com>
2019-01-05 17:27:30 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -394,7 +394,7 @@ bot.on('message', message => {
         break;
                      //CLEAR
                      case "clear":
-                     if (message.member.hasPermission("SPEAK")){
+                     if (message.member.hasPermission("MANAGE_MESSAGES")){
                          message.channel.fetchMessages()
                              .then(function(list){
                                  message.channel.bulkDelete(list);
